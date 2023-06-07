a85e04b422a5ede72f860a8ad5a61aaaa271262f
Kok1ysh <yalovenko.lybomur@gmail.com>
2023-06-07 14:04:15 +0300

    Initial commit

---
+++ shop/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from shop import views
+
+app_name='shop'
+
+urlpatterns=[
+    path('about',views.about_shop),
+    path('contact',views.contacts),
+    path('product/<int:id>', views.product_detail, name='product_detail'),
+    path('<slug:category_slug>/',views.product_list,name='product_list_by_category'),
+    path('', views.product_list,name='product_list')
+]
